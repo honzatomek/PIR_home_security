a15f471e0f8df75edb0a94f4c7051ed6b4dc91b0
honzatomek <rpi3.tomek@protonmail.com>
2020-05-30 22:07:30 +0200

    added comments

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ pir = machine.Pin(13, machine.Pin.IN)
 led = machine.Pin(2, machine.Pin.OUT, value=1)
 
 while True:
+    # check if PIR sensor detected motion = needs a repeatable setting
     if pir.value():
         led.value(0)
+        # this delay should conform to the PIR sensor one
         sleep_ms(5000)
         if not pir.value():
             led.value(1)
